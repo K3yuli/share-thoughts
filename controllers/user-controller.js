0e4d62c6a5e29e345463e7fa50ef7978a7e3ce15
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-03-10 13:11:23 -0500

    routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,6 +6,15 @@ const userController = {
     // get all users
     getAllUser(req, res) {
         User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
@@ -16,6 +25,15 @@ const userController = {
     // & populate thought and friend data
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
         .then(dbUserData => {
             // if no User found send 404
             if(!dbUserData) {
@@ -64,7 +82,5 @@ const userController = {
     // DELETE a user's associated thoughts when deleted (bonus)
 
 };
-
-
 
 module.exports = userController;
